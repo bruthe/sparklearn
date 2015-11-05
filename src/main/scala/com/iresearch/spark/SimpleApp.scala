63d61f267902760d79724eb356ff5498e499bfbf
bruthe <bruthe@163.com>
2015-11-05 19:10:08 +0800

    init project

---
+++ src/main/scala/com/iresearch/spark/SimpleApp.scala
@@ -0,0 +1,12 @@
+package com.iresearch.spark
+
+/**
+  * Created by hadoop on 2015/11/5.
+  */
+object SimpleApp {
+
+  def main(args: Array[String]) {
+    print("hello world ..")
+  }
+
+}
---
+++ src/test/scala/com/iresearch/spark/SimpleAppTest.scala
@@ -0,0 +1,8 @@
+package com.iresearch.spark
+
+/**
+  * Created by hadoop on 2015/11/5.
+  */
+class SimpleAppTest {
+
+}
